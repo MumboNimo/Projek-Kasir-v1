3794a7819758b9de0b114c70923a371a3b39a781
Iko <prinf.honny@gmail.com>
2026-05-07 19:25:09 +0700

    selesai proyek kasir versi 1

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import { createApp } from "vue"; // fungsi pembuat aplikasi Vue
-import App from "./App.vue"; // komponen root (induk semua komponen)
-import "./style.css"; // CSS global (termasuk Tailwind)
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./style.css";
 
 createApp(App).mount("#app");
